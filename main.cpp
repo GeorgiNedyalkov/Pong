4dcd635218708c5a4d6e9476880117c59a3d8603
Georgi Nedyalkov <gnedyalkov94@gmail.com>
2024-05-03 12:25:59 +0300

    process user input and draw a red screen

--- main.cpp
+++ main.cpp
@@ -15,7 +15,13 @@ int main(int argc, char** argv)
 	SDL_Window* window = SDL_CreateWindow("Pong", 100, 100, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
 	if (window == NULL)
 	{
-		printf("Could not create SDL Window: %s", SDL_GetError());
+		printf("Could not create SDL Renderer: %s", SDL_GetError());
+	}
+
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+	if (renderer == NULL)
+	{
+		printf("Could not create an SDL Renderer: %s", SDL_GetError());
 	}
 
 	bool running = true;
@@ -23,18 +29,41 @@ int main(int argc, char** argv)
 
 	while (running)
 	{
+		// Process Input
 		while (SDL_PollEvent(&event) != 0)
 		{
 			switch (event.type)
 			{
 			case SDL_QUIT:
 				running = false;
+				break;
 			}
 		}
+
+		const Uint8* keyboard = SDL_GetKeyboardState(NULL);
+
+
+		if (keyboard[SDL_SCANCODE_ESCAPE])
+		{
+			running = false;
+		}
+
+		// DRAW  the scene
+		// Set the render color
+		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+		// Clear the back buffer and set the draw color
+		SDL_RenderClear(renderer);
+
+		// Swap the front an back buffer
+		SDL_RenderPresent(renderer);
 	}
 
+	// Shutdown Game
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 
 	return 0;
 }
+
+
