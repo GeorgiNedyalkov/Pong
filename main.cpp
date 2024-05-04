9453111048b2d13501af161902f33c7647caa0c4
GeorgiNedyalkov <gnedyalkov94@gmail.com>
2024-05-04 11:19:28 +0300

    add paddle and ball to the game, and paddle movement

--- main.cpp
+++ main.cpp
@@ -4,8 +4,15 @@
 const int WINDOW_WIDTH = 1024;
 const int WINDOW_HEIGHT = 768;
 
+struct Vector2
+{
+	float x;
+	float y;
+};
+
 int main(int argc, char** argv)
 {
+	// INITIALIZE
 	int success = SDL_Init(SDL_INIT_VIDEO);
 	if (success != 0)
 	{
@@ -24,12 +31,27 @@ int main(int argc, char** argv)
 		printf("Could not create an SDL Renderer: %s", SDL_GetError());
 	}
 
-	bool running = true;
-	SDL_Event event;
+	// The number of miliseconds after the Init function
+	Uint32 mTicksCount = SDL_GetTicks();
 
+	bool running = true;
+
+	// Define Paddle position and direction
+	Vector2 paddlePosition;
+	paddlePosition.x = 0;
+	paddlePosition.y = WINDOW_HEIGHT / 2.0f;
+	int paddleDir;
+
+	// Define Ball position and direction
+	Vector2 ballPosition;
+	ballPosition.x = WINDOW_WIDTH / 2.0f;
+	ballPosition.y = WINDOW_HEIGHT / 2.0f;
+	int ballDir;
+
+	// PROCESS INPUT
+	SDL_Event event;
 	while (running)
 	{
-		// Process Input
 		while (SDL_PollEvent(&event) != 0)
 		{
 			switch (event.type)
@@ -42,18 +64,51 @@ int main(int argc, char** argv)
 
 		const Uint8* keyboard = SDL_GetKeyboardState(NULL);
 
-
 		if (keyboard[SDL_SCANCODE_ESCAPE])
 		{
 			running = false;
 		}
 
-		// DRAW  the scene
+		paddleDir = 0;
+		if (keyboard[SDL_SCANCODE_W])
+		{
+			paddleDir -= 1;
+		}
+		else if (keyboard[SDL_SCANCODE_S])
+		{
+			paddleDir += 1;
+		}
+
+		// UPDATE GAME
+
+		// Calculate deltaTime and update the mTicksCount
+		float deltaTime = (SDL_GetTicks() - mTicksCount) / 1000.0f;
+		mTicksCount = SDL_GetTicks();
+
+		if (paddleDir != 0)
+		{
+			paddlePosition.y += paddleDir * 300.0f * deltaTime;
+		}
+
+		// DRAW SCENE 
 		// Set the render color
 		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 
 		// Clear the back buffer and set the draw color
 		SDL_RenderClear(renderer);
+
+		// Set the draw color to white to draw actors
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+
+		// Draw the first paddle with a width of 25 pixels and height of 150 pixels
+		SDL_Rect paddle{ 
+			static_cast<int>(paddlePosition.x),
+			static_cast<int>(paddlePosition.y - (150 / 2.0f)),
+			25,
+			150
+		};
+
+		SDL_RenderFillRect(renderer, &paddle);
 
 		// Swap the front an back buffer
 		SDL_RenderPresent(renderer);
